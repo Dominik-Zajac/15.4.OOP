da6d549367fca7c38ba6185683189b60a933ba12
Wyrafinowany <41593253+Wyrafinowany@users.noreply.github.com>
2018-10-29 20:28:07 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ function Phone(brand, price, color, screen) {
   this.price = price;
   this.color = color;
   this.screen = screen;
-}
+};
 
 Phone.prototype.printInfo = function() {
   console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', the price is ' + this.price + ' and size screen is ' + this.screen + '.');
-}
+};
 
 var GalaxyS6 = new Phone('Samsung', 1850, 'black', '4.90"');
 var iPhone6S = new Phone('Apple', 2250, 'silver', '4.70"');
